8047e4a3031950c55eafa9ba62e979b1793e4233
andrey <dyhazykov@yandex.ru>
2026-09-13 21:58:26 +0300

    add special person operations

--- backend/src/main/java/org/backend/lab1/persons/PersonController.java
+++ backend/src/main/java/org/backend/lab1/persons/PersonController.java
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.backend.lab1.enums.Color;
+import org.backend.lab1.enums.Country;
 import org.backend.lab1.persons.dto.PersonRequest;
 import org.backend.lab1.persons.dto.PersonResponse;
 import org.springframework.http.ResponseEntity;
@@ -16,9 +18,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @Validated
 @Slf4j
@@ -63,6 +67,33 @@ public class PersonController {
             @NotNull(message = "person id required")
             @Positive(message = "person id must be positive")
             @PathVariable Long id) {
+        return null;
+    }
+
+    @DeleteMapping("/delete-by-height")
+    public ResponseEntity<Void> deleteByHeight(@RequestParam(name = "height") Double height) {
+        return null;
+    }
+
+    @GetMapping("/avg-height")
+    public ResponseEntity<Map<String, Double>> getAverageHeight() {
+        return null;
+    }
+
+    @GetMapping("/count-nationality-less-than")
+    public ResponseEntity<Map<String, Long>> countNationalityLessThan(
+            @RequestParam(name = "nationality")
+            Country nationality) {
+        return null;
+    }
+
+    @GetMapping("/count-hair-color")
+    public ResponseEntity<Map<String, Long>> countHairColor(@RequestParam(name = "hair-color") Color color) {
+        return null;
+    }
+
+    @GetMapping("/hair-color-percentage")
+    public ResponseEntity<Map<String, Double>> getHairColorPercentage(@RequestParam(name = "hair-color") Color color) {
         return null;
     }
 }
